0caa6af2eb2dbcc8e9609aeb211c2495b40cbdb3
Marissa Saidy <msaidy@eagles.ewu.edu>
2016-06-06 18:54:28 -0700

    version 4

--- js/scripts.js
+++ js/scripts.js
@@ -1,68 +1,68 @@
-// Animate Star
-
-var $triangle1 = $('#triangle1');
-
-// DOM Element, Duration, CSS Properties
-TweenMax.to($triangle1, 1, {
-    opacity: 0,
-    
-});
-
-var $triangle2 = $('#triangle2');
-
-
-TweenMax.to($triangle2, 2, {
-    opacity: 0,
-    
-});
-
-var $triangle3 = $('#triangle3');
-
-
-TweenMax.to($triangle3, 3, {
-    opacity: 0,
-    
-});
-
-
-var $triangle4 = $('#triangle4');
-
-
-TweenMax.to($triangle4, 4, {
-    opacity: 0,
-    
-});
-
-
-var $triangle5 = $('#triangle5');
-
-
-TweenMax.to($triangle5, 5, {
-    opacity: 0,
-    
-});
-
-
-var $triangle6 = $('#triangle6');
-
-
-TweenMax.to($triangle6, 6, {
-    opacity: 0,
-    
-});
-
-var $triangle7 = $('#triangle7');
-
-
-TweenMax.to($triangle7, 1, {
-    opacity: 0,
-    
-});
-
-var $triangle8 = $('#triangle8');
-
-
-TweenMax.to($triangle8, 1.75, {
-    opacity: 0,
-    
-});
+//// Animate Star
+//
+//var $triangle1 = $('#triangle1');
+//
+//// DOM Element, Duration, CSS Properties
+//TweenMax.to($triangle1, 1, {
+//    opacity: 0,
+//    
+//});
+//
+//var $triangle2 = $('#triangle2');
+//
+//
+//TweenMax.to($triangle2, 2, {
+//    opacity: 0,
+//    
+//});
+//
+//var $triangle3 = $('#triangle3');
+//
+//
+//TweenMax.to($triangle3, 3, {
+//    opacity: 0,
+//    
+//});
+//
+//
+//var $triangle4 = $('#triangle4');
+//
+//
+//TweenMax.to($triangle4, 4, {
+//    opacity: 0,
+//    
+//});
+//
+//
+//var $triangle5 = $('#triangle5');
+//
+//
+//TweenMax.to($triangle5, 5, {
+//    opacity: 0,
+//    
+//});
+//
+//
+//var $triangle6 = $('#triangle6');
+//
+//
+//TweenMax.to($triangle6, 6, {
+//    opacity: 0,
+//    
+//});
+//
+//var $triangle7 = $('#triangle7');
+//
+//
+//TweenMax.to($triangle7, 1, {
+//    opacity: 0,
+//    
+//});
+//
+//var $triangle8 = $('#triangle8');
+//
+//
+//TweenMax.to($triangle8, 1.75, {
+//    opacity: 0,
+//    
+//});
